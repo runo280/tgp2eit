a9b775a58e772e17acb6ea33a4772e9cc472ba67
runo280 <runo280@users.noreply.github.com>
2022-12-03 23:42:37 +0330

    init

--- Main.py
+++ Main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
     message = ""
     for p in db.get_publish_queue():
-        message = p['url'] + "\n\n"
+        message = message  + p['url'] + "\n\n"
     
     r = send_message(message)
     if r['ok']:
